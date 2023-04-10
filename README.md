e6266dd36fb5a54114f94191d983fec743c1958e
Oksana <ksenia.tkachenko.ua@gmail.com>
2023-04-10 18:31:19 -0400

    add brain-calc game

--- README.md
+++ README.md
@@ -5,5 +5,10 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/8934dc09aa0163baa1cd/maintainability)](https://codeclimate.com/github/0ksanaTkachenko/frontend-project-44/maintainability)
 
 <!-- installation and game process -->
+<!-- brain-even game -->
 
 https://asciinema.org/a/575427
+
+<!-- brain-calc game -->
+
+https://asciinema.org/a/576463
---
+++ src/games/brain-calc-game.js
@@ -0,0 +1,47 @@
+import {
+  helloFunction,
+  askQuestion,
+  generateNumber,
+  isCorrect,
+  shouldStop,
+  congratulations,
+} from "../index.js";
+
+export const playCalcGame = () => {
+  helloFunction();
+  console.log("What is the result of the expression?");
+
+  const operations = ["+", "-", "*"];
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber1 = generateNumber(10);
+    const randomNumber2 = generateNumber(10);
+
+    // random operator generation
+    const randomIndex = generateNumber(operations.length);
+    const operation = operations[randomIndex];
+
+    // correct answer
+    const correctAnswer = eval(
+      `${randomNumber1} ${operation} ${randomNumber2}`
+    );
+
+    // Ask a user a question
+    const userAnswer = askQuestion(
+      `Question: ${randomNumber1} ${operation} ${randomNumber2} `
+    );
+
+    // checking for the correctness of the answer
+    isCorrect(correctAnswer, userAnswer);
+
+    // Loop exit on wrong answer
+    if (shouldStop) {
+      break;
+    }
+
+    // congratulations on victory
+    if (i === 2) {
+      congratulations();
+    }
+  }
+};
